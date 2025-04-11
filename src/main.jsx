0b8f4c7cccc5a3fad6851012ccf03384eab29522
Faysal64 <fdriouach10@gmail.com>
2025-04-11 09:16:42 +0200

    commit110425

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+// src/main.jsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent';
+import SignIn from './components/SignIn';
+import './HomePage.css';
+
+
+
+function App() {
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById('root')).render(<App />);
